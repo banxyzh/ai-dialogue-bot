2f02854700b23551034cee129d0bfdf8c7d033d1
banxyzh <banxyzh@proton.me>
2022-09-11 12:48:45 +0000

    Updated steps.html

--- grucell_cond.py
+++ grucell_cond.py
@@ -29,3 +29,31 @@ class CondWrapper(RNNCell):
   def __call__(self, inputs, state, scope=None):
     output, res_state = self._cell(inputs, state, self._context)
     return output, res_state
+
+class GRUCellCond(RNNCell):
+  """Gated Recurrent Unit cell (cf. http://arxiv.org/abs/1406.1078)."""
+
+  def __init__(self, num_units, input_size=None, activation=tanh, reuse=None):
+    if input_size is not None:
+      logging.warn("%s: The input_size parameter is deprecated.", self)
+    self._num_units = num_units
+    self._activation = activation
+    self._reuse = reuse
+
+  @property
+  def state_size(self):
+    return self._num_units
+
+  @property
+  def output_size(self):
+    return self._num_units
+
+  def __call__(self, inputs, state, context, scope=None):
+    """Gated recurrent unit (GRU) with nunits cells."""
+    with _checked_scope(self, scope or "gru_cell", reuse=self._reuse):
+      with vs.variable_scope("gates"):  # Reset gate and update gate.
+        # We start with bias of 1.0 to not reset and not update.
+        value = sigmoid(_linear(
+          [inputs, state, context], 2 * self._num_units, True, 1.0))
+        r, u = array_ops.split(
+            value=value,
