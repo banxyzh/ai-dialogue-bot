a7a63ff14685c1512f9b8bd7442f60838410b9f0
banxyzh <banxyzh@proton.me>
2022-10-18 21:35:29 +0000

    Fixed broken test.

--- model.py
+++ model.py
@@ -4,4 +4,4 @@ from tensorflow.contrib import seq2seq
 from tensorflow.contrib.rnn import MultiRNNCell, DropoutWrapper, GRUCell
 from tensorflow.python.ops import array_ops
 from tensorflow.python.ops import init_ops
-from tensorflow.python.ops.nn import bidirection
+from tensorflow.python.ops.nn import bidirectional_dynamic_rnn
