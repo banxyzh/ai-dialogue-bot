3dd50c6d2013076a401c6cc0af8ae4fdba963049
banxyzh <banxyzh@proton.me>
2022-10-30 21:13:24 +0000

    dev update

--- model.py
+++ model.py
@@ -5,4 +5,5 @@ from tensorflow.contrib.rnn import MultiRNNCell, DropoutWrapper, GRUCell
 from tensorflow.python.ops import array_ops
 from tensorflow.python.ops import init_ops
 from tensorflow.python.ops.nn import bidirectional_dynamic_rnn
-from tensorflow.python.layers import core as layers_
+from tensorflow.python.layers import core as layers_core
+from tensorflow.contrib
