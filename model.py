772af778fcb5de01a27c10b1291ef3440a28d552
banxyzh <banxyzh@proton.me>
2023-01-29 10:25:13 +0000

    Update bag_of_tasks.md

--- model.py
+++ model.py
@@ -33,4 +33,5 @@ class DialogueModel(object):
     self._max_seq_length = max_seq_length
     self._unk_token_id = unk_token_id
     self._keep_prob = keep_prob
-    self._temperature = 
+    self._temperature = temperature
+    self
