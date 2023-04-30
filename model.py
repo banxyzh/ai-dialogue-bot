50555cffa7caf6a83f556cdf6ecb7f97922cd1aa
banxyzh <banxyzh@proton.me>
2023-04-30 09:43:20 +0000

    mit

--- model.py
+++ model.py
@@ -58,4 +58,6 @@ class DialogueModel(object):
       bw_cell = GRUCell(emb_size)
       if self._keep_prob < 1 and not infer:
         fw_cell = DropoutWrapper(fw_cell, output_keep_prob=self._keep_prob)
-        bw_cell = DropoutWrapper(bw_cell, output_keep_prob=self._keep_p
+        bw_cell = DropoutWrapper(bw_cell, output_keep_prob=self._keep_prob)
+
+    with tf.variable
