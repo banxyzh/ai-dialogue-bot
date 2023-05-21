e0a5916f6effd6e5df457f9a165914649fac2f82
banxyzh <banxyzh@proton.me>
2023-05-21 22:50:09 +0000

    Automated merge for py3k testing

--- model.py
+++ model.py
@@ -63,4 +63,4 @@ class DialogueModel(object):
     with tf.variable_scope("context", initializer=glorot()):
       ctx_cell = GRUCell(memory_size * 2)
       self.ctx_w = tf.get_variable("context_w", [memory_size * 2, memory_size])
-      self.ctx_b = tf.get
+      self.ctx_b = tf.get_variable("context_b", [mem
