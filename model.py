248c65bf81ce62efe68bfa3abbbd5c241cc80daa
banxyzh <banxyzh@proton.me>
2023-06-04 09:51:53 +0000

    Fixed project name

--- model.py
+++ model.py
@@ -64,4 +64,4 @@ class DialogueModel(object):
       ctx_cell = GRUCell(memory_size * 2)
       self.ctx_w = tf.get_variable("context_w", [memory_size * 2, memory_size])
       self.ctx_b = tf.get_variable("context_b", [memory_size], initializer=init_ops.zeros_initializer())
-      self.initial_state = ctx_cell.zero_sta
+      self.initial_state = ctx_cell.zero_state(self._batch_size, tf.fl
