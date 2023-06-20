3b41296296d714e5eebf3483aecc1c51b548b901
banxyzh <banxyzh@proton.me>
2023-06-20 21:55:43 +0000

    Delete phpunit.xml

--- model.py
+++ model.py
@@ -81,4 +81,5 @@ class DialogueModel(object):
     print("parameter size:", _count_param_size(tvars))
 
     grads, _ = tf.clip_by_global_norm(tf.gradients(self.loss, tvars), grad_clip)
-    optimizer = tf.train.Adam
+    optimizer = tf.train.AdamOptimizer(learning_rate)
+  
