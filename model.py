5d0786e796b62bcb16c8a06306a5d05d375c95b0
banxyzh <banxyzh@proton.me>
2023-07-03 15:44:35 +0000

    compress and deploy

--- model.py
+++ model.py
@@ -95,4 +95,4 @@ class DialogueModel(object):
       final_loss = seq2seq.sequence_loss(out_logits_slice, out_data_slice, length_mask)
       return final_loss
 
-  def seq2seq(self, inputs, fw_cell, bw_cell, ctx_cell,
+  def seq2seq(self, inputs, fw_cell, bw_cell, ctx_cell, dec_cell, reuse=F
