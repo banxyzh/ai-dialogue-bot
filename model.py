592bfbe3f5a7cf127a1db69d847c90aa6d951c6d
banxyzh <banxyzh@proton.me>
2023-07-05 07:36:36 +0000

    Edited readme

--- model.py
+++ model.py
@@ -97,3 +97,5 @@ class DialogueModel(object):
 
   def seq2seq(self, inputs, fw_cell, bw_cell, ctx_cell, dec_cell, reuse=False):
     with tf.variable_scope("seq2seq") as scope:
+      if reuse:
+      
