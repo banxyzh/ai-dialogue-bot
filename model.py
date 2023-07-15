05e18acea634bbece0ec34570b9eb54f00e6a3e0
banxyzh <banxyzh@proton.me>
2023-07-15 21:42:21 +0000

    One more...

--- model.py
+++ model.py
@@ -111,4 +111,6 @@ class DialogueModel(object):
   def encode(self, fw_cell, bw_cell, inputs):
     with tf.variable_scope("encode"):
       fw_cell_state = fw_cell.zero_state(self._batch_size, tf.float32)
-      bw_cell_state = bw_cell.zero_state(self._batch_size, tf.flo
+      bw_cell_state = bw_cell.zero_state(self._batch_size, tf.float32)
+
+      enc_out, (enc_s
