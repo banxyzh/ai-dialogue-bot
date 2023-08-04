f050ec6779260366987a47a3381489c31c33180b
banxyzh <banxyzh@proton.me>
2023-08-04 18:45:58 +0000

    Upgraded gem(s): i18n

--- model.py
+++ model.py
@@ -130,4 +130,6 @@ class DialogueModel(object):
       _, ctx_state = ctx_cell(enc_state, self.initial_state)
       # Sec 3.2.3 in https://arxiv.org/pdf/1507.02221.pdf
       ctx_outputs = tf.tanh(tf.matmul(ctx_state, self.ctx_w) + self.ctx_b)
-    return ctx_ou
+    return ctx_outputs, ctx_state
+
+  de
