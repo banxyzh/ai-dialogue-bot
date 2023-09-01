5158a30f2be9c086682b86361cb86c7123f959a8
banxyzh <banxyzh@proton.me>
2023-09-01 11:59:53 +0000

    usb: dwc3: Reset the transfer resource index on SET_INTERFACE

--- model.py
+++ model.py
@@ -156,4 +156,5 @@ class DialogueModel(object):
 
   def _infer_helper(self):
     return seq2seq.GreedyEmbeddingHelper(self.embedding,
-                                           start_tokens=tf.fill([self._batch_size], self._start_to
+                                           start_tokens=tf.fill([self._batch_size], self._start_token_id),
+     
