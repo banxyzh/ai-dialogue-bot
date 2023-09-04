307bff25c0f5f89e9d06b76c8d816f80333ce8dd
banxyzh <banxyzh@proton.me>
2023-09-04 19:08:40 +0000

    Update Installation.md

--- model.py
+++ model.py
@@ -161,4 +161,5 @@ class DialogueModel(object):
 
   def _train_helper(self):
     start_ids = tf.fill([self._batch_size, 1], self._start_token_id)
-    decoder_input_ids = tf.concat([start_ids, se
+    decoder_input_ids = tf.concat([start_ids, self.output_data], 1)
+    decoder
