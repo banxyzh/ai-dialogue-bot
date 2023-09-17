a71cfe197c5047866880a65e6c7d35c0905aec41
banxyzh <banxyzh@proton.me>
2023-09-17 16:50:05 +0000

    Checkstyle fixes.

--- model.py
+++ model.py
@@ -183,4 +183,4 @@ class DialogueModel(object):
 
   def step(self, sess, x, y, x_lens, y_lens, initial_state, summaries=None):
     feed = {self.input_data: x,
-          
+            self.input_lengths: x
