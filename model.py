83d7c5ebee2c30645327326245f17a045f7cbefa
banxyzh <banxyzh@proton.me>
2023-09-20 22:01:03 +0000

    trunk merged

--- model.py
+++ model.py
@@ -189,4 +189,5 @@ class DialogueModel(object):
     fetches = {"outputs": self.outputs,
                "output_ids": self.output_ids,
                "final_state": self.final_state,
-               "loss":
+               "loss": self.loss,
+           
