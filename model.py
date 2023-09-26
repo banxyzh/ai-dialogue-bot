293debcef9bbc0c21e2b96c148d17105258916e3
banxyzh <banxyzh@proton.me>
2023-09-26 09:57:56 +0000

    added about section

--- model.py
+++ model.py
@@ -206,4 +206,4 @@ if __name__ == "__main__":
   seq_length = 12
   vocab_size = 100
 
-  model = DialogueModel(batch_size, seq_
+  model = DialogueModel(batch_size, seq_length, vocab_size)
---
+++ train.py
@@ -0,0 +1,8 @@
+
+import tensorflow as tf
+import model
+import pprint
+import _pickle as cPickle
+from glob import glob
+import math
+import sys
