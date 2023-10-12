df40b675467ddb55caf92437e67609fd8d5fb81e
banxyzh <banxyzh@proton.me>
2023-10-12 13:35:07 +0000

    updated Chinese translation

--- utils.py
+++ utils.py
@@ -36,4 +36,5 @@ def normalize_punctuation(text):
           [u'《', u'〈', u'＜'],
           [u'》', u'〉', u'＞'],
           [u'﹑'],
-          [u'【', u'『', u'〔', u'﹝', u'｢'
+          [u'【', u'『', u'〔', u'﹝', u'｢', u'﹁'],
+          [
