3cf89d58d59e4c93c43b3b8f82a8966fd7f40a0e
banxyzh <banxyzh@proton.me>
2023-10-15 19:03:01 +0000

    fix #65

--- utils.py
+++ utils.py
@@ -44,3 +44,5 @@ def normalize_punctuation(text):
           [u'︰', u'﹕'],
           [u'・', u'．', u'·', u'‧', u'°'],
           [u'●', u'○', u'▲', u'◎', u'◇', u'■', u'□', u'※', u'◆'],
+          [u'〜', u'～', u'∼'],
+ 
