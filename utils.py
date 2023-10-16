8e257ecd20809299db7e6cab4cc931a64f7fde9d
banxyzh <banxyzh@proton.me>
2023-10-16 19:11:59 +0000

    NR-250 : Latest fixes for 1.0 beta release

--- utils.py
+++ utils.py
@@ -45,4 +45,4 @@ def normalize_punctuation(text):
           [u'・', u'．', u'·', u'‧', u'°'],
           [u'●', u'○', u'▲', u'◎', u'◇', u'■', u'□', u'※', u'◆'],
           [u'〜', u'～', u'∼'],
- 
+          [u'︱', u'
